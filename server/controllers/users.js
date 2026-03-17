60bb8b135c64e009ab90cd3190b0ce73d493f915
jpangi <jacob.Pangilinan10@gmail.com>
2026-03-17 12:02:34 -0700

    updated signup controller and started login controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -15,17 +15,32 @@ const signUp = async (req, res) => {
     const emailExists = await User.findOne({ email });
     if (emailExists)
       return res.status(400).json({ message: "Email already in use" });
+
+    //hashes the password from the user
     const hashedPassword = bcrypt.hashSync(password, SALT_LENGTH);
     const user = await User.create({
       email,
       password: hashedPassword,
     });
+
+    //calls the createToken function and passes in user_id from user model
     const token = createToken(user.id);
     console.log(token);
-    res.json({ message: "User created and logged in", token, user });
+    res.json({
+      message: "User created and logged in",
+      token,
+      user,
+    });
   } catch (error) {
-    return res.status(400).json({ message: "Error with signup", error });
+    return res.status(400).json({
+      message: "Error with signup",
+      error,
+    });
   }
 };
 
+/*--------LOGIN CONTROLLER--------*/
+const login = async (req, res) => {
+  const { email, password } = req.body;
+};
 module.exports = { signUp };
